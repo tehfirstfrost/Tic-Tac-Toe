4eda2888a6e2fe6724dae4b3a5636ef510da2af2
tehfirstfrost <tehfirstfrost@gmail.com>
2022-08-12 10:19:15 -0500

    .

---
+++ index.js
@@ -0,0 +1,168 @@
+
+
+let p1;
+let p2;
+
+const tabulation ={
+    count: 0,
+
+    win: function(){
+        if(this['row one'] === "X" && this['row two'] === "X" && this['row three'] === "X"){
+            tabulation.winner(p1);
+        }else if(this['row four'] === "X" && this['row five'] === "X" && this['row six'] === "X"){
+            tabulation. winner(p1);
+        }else if(this['row seven'] === "X" && this['row eight'] === "X" && this['row nine'] === "X"){
+            tabulation.winner(p1);
+        }else if(this['row one'] === "X" && this['row four'] === "X" && this['row seven'] === "X"){
+            tabulation. winner(p1);
+        }else if(this['row two'] === "X" && this['row five'] === "X" && this['row eight'] === "X"){
+            tabulation.winner(p1);
+        }else if(this['row three'] === "X" && this['row six'] === "X" && this['row nine'] === "X"){
+            tabulation.winner(p1);
+        }else if(this['row one'] === "X" && this['row five'] === "X" && this['row nine'] === "X"){
+            tabulation. winner(p1);
+        }else if(this['row seven'] === "X" && this['row five'] === "X" && this['row three'] === "X"){
+            tabulation.winner(p1);
+        }else if(this['row one'] === "O" && this['row two'] === "O" && this['row three'] === "O"){
+            tabulation.winner(p2);
+        }else if(this['row four'] === "O" && this['row five'] === "O" && this['row six'] === "O"){
+            tabulation.winner(p2);
+        }else if(this['row seven'] === "O" && this['row eight'] === "O" && this['row nine'] === "O"){
+            tabulation.winner(p2);
+        }else if(this['row one'] === "O" && this['row four'] === "O" && this['row seven'] === "O"){
+            tabulation.winner(p2);
+        }else if(this['row two'] === "O" && this['row five'] === "O" && this['row eight'] === "O"){
+            tabulation.winner(p2);
+        }else if(this['row three'] === "O" && this['row six'] === "O" && this['row nine'] === "O"){
+            tabulation.winner(p2);
+        }else if(this['row one'] === "O" && this['row five'] === "O" && this['row nine'] === "O"){
+            tabulation.winner(p2);
+        }else if(this['row seven'] === "O" && this['row five'] === "O" && this['row three'] === "O"){
+            tabulation.winner(p2);
+        }else{
+            tabulation.count++;
+            tabulation.cat();
+        }
+    },
+    cat: function(){
+        if(tabulation.count >= 9){
+            const result = document.getElementById('result');
+            result.innerText = `Its a Cats Game ${p1} is tied with ${p2}`;
+            const board = document.getElementById('board');
+            const cover = document.createElement('div');
+            cover.classList.add('cover');
+            board.appendChild(cover);
+        }
+    },
+    clear: function(){
+        location.reload();  
+    },
+    winner: function(player){
+
+        const result = document.getElementById('result');
+        result.innerText = `The Winner is: ${player}`;
+        const board = document.getElementById('board');
+        const cover = document.createElement('div');
+        cover.classList.add('cover');
+        board.appendChild(cover);
+    },
+    rotation: function(counter){
+        const turn = document.getElementById('result');
+
+        if(counter%2 === 0){
+            turn.innerText = `${p1}'s turn`
+        }else if(counter%2 !== 0){
+            turn.innerText = `${p2}'s turn`
+        }
+    },
+    play: function(){
+        box = document.querySelectorAll('.row');
+        p1 = document.getElementById('player1').value || 'Player 1';
+        p2 = document.getElementById('player2').value || 'Player 2';
+        let counter = 0;
+
+        tabulation.rotation(counter);
+    
+        box.forEach(box => box.addEventListener('click', ()=>{
+
+                if(counter%2 === 0){
+                    tabulation[box.className] = "X";
+                    box.style.cssText = "background-color: blue";
+                    box.innerText = "X";
+                    ++counter;
+                    console.log("even");
+                    tabulation.rotation(counter);
+                }else if(counter%2 !== 0){
+                    tabulation[box.className] = "O";
+                    box.style.cssText = "background-color: red";
+                    box.innerText = "O";
+                    ++counter;
+                    console.log('odd')
+                    tabulation.rotation(counter);
+                }
+          
+        console.log(tabulation);
+        tabulation.win();
+        }));
+    
+    }
+}
+
+
+
+
+// function play(){
+//     let counter = 0;
+//     p1 = document.getElementById('player1').value;
+//     p2 = document.getElementById('player2').value;
+//     const box = document.querySelectorAll('.row');
+//     const boxArray = Array.from(box);
+
+//     tabulation.clear();
+//     tabulation.rotation(counter);
+
+//     box.forEach(box => box.addEventListener('click', ()=>{
+//         if(counter%2 === 0){
+//             tabulation[box.className] = "X";
+//             box.style.cssText = "background-color: blue";
+//             box.innerText = "X";
+//             ++counter;
+//             tabulation.rotation(counter);
+//         }else if(counter%2 !== 0){
+//             tabulation[box.className] = "O";
+//             box.style.cssText = "background-color: red";
+//             box.innerText = "O";
+//             ++counter;
+//             tabulation.rotation(counter);
+//         }    
+//     console.log(tabulation);
+//     tabulation.win();
+//     }));
+
+// }
+
+
+// function clear(){
+//     const p = document.getElementsByClassName('row');
+//     for(let i = 0; i < p.length; i++){
+//         p[i].innerText = "";
+//         p[i].style.cssText = "background-color: antiquewhite;"
+//     }
+
+// }
+
+// function winner(player){
+//     const result = document.getElementById('result');
+//     result.innerText = `The Winner is: ${player}`;
+// }
+
+// function rotation(counter){
+//     const turn = document.getElementById('result');
+
+//     if(counter%2 === 0){
+//         turn.innerText = `${p1}'s turn`
+//     }else if(counter%2 !== 0){
+//         turn.innerText = `${p2}'s turn`
+//     }
+// }
+
